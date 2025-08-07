e390dbe30a3d724a6d9ed49ab44e168cad1802b3
Lealdeb <hualadebi@gmail.com>
2025-08-07 00:49:06 -0400

    act aip.js

--- panel-xml-actualizado/src/services/api.js
+++ panel-xml-actualizado/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8001', // Cambia si usas otro puerto
+  baseURL: "https://factorg.onrender.com", // Cambia si usas otro puerto
 });
 
 export const uploadXML = (formData) =>
