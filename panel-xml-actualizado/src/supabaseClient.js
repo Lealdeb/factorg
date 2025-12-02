b5d125a22b9e6f273cf51eda15d663894ac06756
Lealdeb <hualadebi@gmail.com>
2025-12-02 04:52:53 -0300

    NF: gestion de usuarios FrontEnd3

--- panel-xml-actualizado/src/supabaseClient.js
+++ panel-xml-actualizado/src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://saqsmmkzxljrnsmcplxt.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhcXNtbWt6eGxqcm5zbWNwbHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MjA3OTAsImV4cCI6MjA2MzQ5Njc5MH0.BCkF3X9QqAgcEcUAmGbH6tNj1uF0Xxs3NXqxgXdQkyA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
