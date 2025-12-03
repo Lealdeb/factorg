e3e65e1286b2d92417bcf19d83f0a424d8358154
Lealdeb <hualadebi@gmail.com>
2025-12-03 13:52:56 -0300

    NF: gestion de usuarios FrontEnd8

--- panel-xml-actualizado/src/services/api.js
+++ panel-xml-actualizado/src/services/api.js
@@ -34,3 +34,8 @@ api.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+export const apiGet = (url, config) => api.get(url, config);
+export const apiPost = (url, data, config) => api.post(url, data, config);
+export const apiPut = (url, data, config) => api.put(url, data, config);
+export const apiDelete = (url, config) => api.delete(url, config);
