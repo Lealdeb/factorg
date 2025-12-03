91bda1bd81d3866347a65d69cdbe5ff07f88ca3b
Lealdeb <hualadebi@gmail.com>
2025-12-03 14:01:02 -0300

    NF: gestion de usuarios FrontEnd9

--- panel-xml-actualizado/src/supabaseClient.js
+++ panel-xml-actualizado/src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'postgresql://postgres.saqsmmkzxljrnsmcplxt:0739Airg----@aws-0-us-east-2.pooler.supabase.com:5432/postgres';
+const supabaseUrl = 'https://saqsmmkzxljrnsmcplxt.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhcXNtbWt6eGxqcm5zbWNwbHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MjA3OTAsImV4cCI6MjA2MzQ5Njc5MH0.BCkF3X9QqAgcEcUAmGbH6tNj1uF0Xxs3NXqxgXdQkyA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
